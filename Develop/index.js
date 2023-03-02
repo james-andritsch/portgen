a9894e823b50e5f09f9736db6dc284a94bdb270e
James <andritjm@gmail.com>
2023-03-02 16:03:32 -0600

    worked on index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,27 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'proTitle',
+        message: 'What would you like your project to be named?',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('You must enter a valid title for this project.');
+                return false;
+            }
+        }
+    },
+    {
+        
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
