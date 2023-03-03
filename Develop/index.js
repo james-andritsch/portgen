4a396a80e6faaf7258caeb9429c38bbb9ada7504
James <andritjm@gmail.com>
2023-03-02 18:25:53 -0600

    fixed generateMarkdown.js

--- Develop/index.js
+++ Develop/index.js
@@ -19,15 +19,171 @@ const questions = [
         }
     },
     {
-        
-    }
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description for this project.',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter a description.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide a instructions on how to install this project.',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter a instructions.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide a usage description for this project.',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter a usage description.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'Please provide a testing instructions for this project.',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter a testing instructions.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'License options are below. Choose from the list.',
+        default: "none",
+        choices: [
+            {
+                name: 'MIT License',
+                value: {
+                    name: 'MIT License',
+                    badge: 'MIT',
+                    link: 'https://opensource.org/licenses/MIT'
+                }
+            },
+            {
+                name: 'ISC License',
+                value: {
+                    name: 'ISC License',
+                    badge: 'ISC',
+                    link: 'https://opensource.org/licenses/ISC'
+                }
+            },
+            {
+                name: 'Apache License 2.0',
+                value: {
+                    name: 'Apache License 2.0',
+                    badge: 'Apache 2.0',
+                    link: 'https://www.apache.org/licenses/LICENSE-2.0'
+                }
+            },
+            {
+                name: 'GNU GPLv3',
+                value: {
+                    name: 'GNU General Public License v3.0',
+                    badge: 'GNU_GPLv3',
+                    link: 'https://www.gnu.org/licenses/gpl-3.0.en.html'
+                }
+            },
+            {
+                name: 'Mozilla Public License 2.0',
+                value: {
+                    name: 'Mozilla Public License 2.0',
+                    badge: 'MPL_2.0',
+                    link: 'https://www.mozilla.org/en-US/MPL/2.0/'
+                }
+            },
+            'none'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Please provide your gGithub username.',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter your Github username.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide your email address',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('You must enter a valid email address.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Please provide contribution guidelines for this project.',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('You must enter valid contribution guidelines for this project.');
+                return false;
+            }
+        }
+    },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//  function to write README file
+function writeToFile(data) {
+    return new Promise ((resolve, reject) => {
+        fs.writeFile(`./dist/README.md`, data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve ({
+                ok: true,
+                message: 'Your README has been created! Check /dist to see your new README file.'
+            })
+        })
+    })
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+//  function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => generateMarkdown(answers))
+    .then(markdown => writeToFile(markdown))
+    .then(writeFileResponse => console.log(writeFileResponse.message))
+    .catch(err => console.log(err))
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,78 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+//  function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  function liceColor(license) {
+    if (license.name === 'MIT License') {
+      return 'yellow.svg';
+    } else if (license.name === 'Mozilla Public License 2.0') {
+      return 'brightgreen.svg';
+    } else if (license.name ==='Boost Software License 1.0') {
+      return 'lightblue.svg';
+    } else {
+      return 'blue.svg';
+    }
+  }
+  if (license === 'none') {
+    return '';
+  } else {
+    return `[![License](https://img.shields.io/badge/License-${license.badge}-${liceColor(license)})](https://opensource.org/licenses/${license.badge})`;
+  }
+}
 
-// TODO: Create a function that returns the license link
+// function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'none') {
+    return '';
+  } else {
+    return `## License [${license.name}]({$license.link})`;
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+// function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'none') {
+    return '';
+  } else {
+    return `## License [${license.name}](${license.link})`;
+  }
+}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+//  generates markdown for README
+function generateMarkdown(response) {
+  return `# ${response.proTitle}
+  ${renderLicenseBadge(response.license)}
+  ## Description:
+  ${response.description}
 
+  ## Table of Contents:
+
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[Contributing](#contributing)
+  -[Tests](#tests)
+  -[Questions](#questions)
+  ${renderLicenseLink(response.license)}
+
+  ## Installation
+  ${response.installation}
+
+  ## Usage
+  ${response.usage}
+
+  ## Contributing
+  ${response.contributions}
+
+  ## Tests
+  ${response.testing}
+
+  ## Questions
+  If you have any questions, you may contact me using the following links:
+  -Please visit my [Github](https://github.com/${response.username})
+  -Please email me at [$response.email](mailto:${response.email})
+
+  ${renderLicenseSection(response.license)}
 `;
 }
 
